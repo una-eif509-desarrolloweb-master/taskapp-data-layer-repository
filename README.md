cd90fbcb2ef2a9882a42ca608f94a3b4e8def4f4
Maikol Guzman <mikeguzman@gmail.com>
2021-09-03 08:51:32 -0600

    Created the new version with Repository

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-taskapp-data-layer-repository
+# Data Layer
+
+ToDo app to manage task, this example is the data layer using Spring Boot and Hibernate with Gradle.
+
+## Wireframes
+
+![](diagrams/wireframes.jpeg)
+
+## Entity Relationship Diagram
+
+In this plan, we will have different entities and relationship between them.
+
+### Initial Plan
+
+![er-diagram](diagrams/er-diagram.jpeg)
+
+### Actual Design
+
+![](diagrams/er-diagram-real.png)
---
+++ src/main/java/cr/una/taskapp/backend/model/Task.java
@@ -0,0 +1,51 @@
+/*
+ *
+ * Universidad Nacional de Costa Rica  2020
+ *
+ * mike@guzmanalan.com
+ *
+ */
+
+package cr.una.taskapp.backend.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Entity for Task
+ */
+@Entity
+@Table( name = "task" )
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    // Properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_task")
+    private Long idTask;
+    private String title;
+    private String notes;
+    @Temporal(TemporalType.DATE)
+    private Date createDate;
+    @Temporal(TemporalType.DATE)
+    private Date dueDate;
+
+    // Relationship definition
+    @ManyToOne
+    @JoinColumn(name = "id_priority", nullable = false)
+    @Getter @Setter
+    private Priority priority;
+    @ManyToOne
+    @JoinColumn(name = "id_status", nullable = false)
+    @Getter @Setter
+    private Status status;
+    @ManyToOne
+    @JoinColumn(name = "id_user", nullable = false)
+    @Getter @Setter
+    private User user;
+}
